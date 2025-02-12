983ac8fa53a4fc86e150b4eb17d1e71defea03ae
Metotron <metotron1@yahoo.com>
2025-02-12 22:49:43 +0300

    Рефакторинг

--- main.js
+++ main.js
@@ -1,38 +1,25 @@
 function validateINN(number) {
-	var parse, n;
+	// Разрешаем использовать пробельные символы в номере
+	number = number.replace(/\s/g, '')
 
-	//Разрешаем использовать пробельные символы в номере
-	number = number.replace(/\s/g, '');
+	// Проверка длины, она может быть только 10 или 12 символов
+	if (!/^([0-9]{10}|[0-9]{12})$/.test(number))
+		return false
 
-	//Проверка длины
-	if (!/^([0-9]{10}|[0-9]{12})$/.test(number)) { return false; }
+	// Проверка ИНН для юр. лиц
+	if (number.length == 10) {
+		const parse = number.match(/^(.{9})(.)$/)  // 9 цифр и одна контрольная
+		const n = parse[1].split('')
+		const checkCode = ((2*n[0] + 4*n[1] + 10*n[2] + 3*n[3] + 5*n[4] + 9*n[5] + 4*n[6] + 6*n[7] + 8*n[8]) % 11) % 10
 
-	//Проверка контрольных цифр
-	if (number.length == 10)
-	{
-		parse = number.match(/^(.{9})(.)$/);
-		n     = parse[1].split('');
-
-		var controlCode = parse[2],
-		    checkCode   = ((2*n[0] + 4*n[1] + 10*n[2] + 3*n[3] + 5*n[4] + 9*n[5] + 4*n[6] + 6*n[7] + 8*n[8]) % 11) % 10;
-
-		if (checkCode != controlCode) {
-			return false;
-		}
-	}
-	else
-	{
-		parse = number.match(/^(.{10})(.)(.)$/);
-		n     = parse[1].split('');
-		var controlCode1 = parse[2],
-			controlCode2 = parse[3],
-			checkCode1   = ((7*n[0] + 2*n[1] + 4*n[2] + 10*n[3] + 3*n[4] + 5*n[5] + 9*n[6] + 4*n[7] + 6*n[8] + 8*n[9]) % 11) % 10,
-			checkCode2   = ((3*n[0] + 7*n[1] + 2*n[2] + 4*n[3] + 10*n[4] + 3*n[5] + 5*n[6] + 9*n[7] + 4*n[8] + 6*n[9] + 8*checkCode1) % 11) % 10;
-
-		if (checkCode1 != controlCode1 || checkCode2 != controlCode2) {
-			return false;
-		}
+		return checkCode == parse[2]
 	}
 
-	return true;
+	// проверка ИНН для физ. лиц (12 символов)
+	const parse = number.match(/^(.{10})(.)(.)$/)  // 10 цифр и две контрольных
+	const n = parse[1].split('')
+	const checkCode1 = ((7*n[0] + 2*n[1] + 4*n[2] + 10*n[3] + 3*n[4] + 5*n[5] + 9*n[6] + 4*n[7] + 6*n[8] + 8*n[9]) % 11) % 10
+	const checkCode2 = ((3*n[0] + 7*n[1] + 2*n[2] + 4*n[3] + 10*n[4] + 3*n[5] + 5*n[6] + 9*n[7] + 4*n[8] + 6*n[9] + 8*checkCode1) % 11) % 10
+
+	return checkCode1 == parse[2] && checkCode2 == parse[3]
 }
